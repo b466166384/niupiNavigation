4832f0bcf48361633e093f1025ef986fa675ec9d
b466166384 <466166384@qq.com>
2020-07-09 15:01:50 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var cookie_name = "bgImg";
+var imgUrl = null;
+document.onreadystatechange = function() {
+	if (document.readyState = "complete") {
+		if (imgUrl == null) {
+			imgUrl = $.cookie('bgImg');
+		}
+		if (imgUrl != null) {
+			document.getElementById("bg_div_img").src = imgUrl;
+		}
+	}
+};
+
+
+
+//获取和设置背景图片
+function setLogoImg() {
+	var txt = document.getElementById("bg_input").value;
+	console.log(txt);
+	document.cookie = "bgImg=" + txt;
+	document.getElementById("bg_div_img").src = txt;
+};
+
+
