879902cb20fa4f8228f53efd1a7901c235aa1606
b466166384 <466166384@qq.com>
2021-11-05 14:17:01 +0800

    Update index_js.js

--- js/index_js.js
+++ js/index_js.js
@@ -1,17 +1,50 @@
 var y
 var str;
 var a1;
+var type;
 
 function btn() {
+	isBrowser();
+	console.log(type);
 	var x = document.getElementById("input")
 	y = x.value
 	$('#shu').html(y)
 	$('tr').hide();
 	a1 = document.getElementsByTagName("img");
+	if (type === "firefox") {
+	var arrayObj = new Array();
+	arrayObj.push(a1.length-1 + ' \n' + '</br>');
+		for (i = 0; i < a1.length; i++) {
+			if (i == 0) {
+
+			} else {
+				arrayObj.push(a1[i].attributes.file.value + ' \n' + '</br>')
+			}
+		}
+	} else {
+		var arrayObj = new Array();
+		arrayObj.push(a1.length + ' \n' + '</br>');
+		for (i = 0; i < a1.length; i++) {
+			arrayObj.push(a1[i].attributes.file.value + ' \n' + '</br>')
+		}
+	}
+	//消除分割符的逗号
+	str = arrayObj.toString().replace(/,/g, '');
+	$("#shu").html(str);
+	document.getElementById("input").value = ""
+}
+
+//ja.hentai-cosplay
+function jaHentaiCos(){
+	var x = document.getElementById("input")
+	y = x.value
+	$('#shu').html(y)
+	$('tr').hide();
+	a1 = document.getElementsByTagName("amp-img");
 	var arrayObj = new Array();
 	arrayObj.push(a1.length + ' \n' + '</br>');
 	for (i = 0; i < a1.length; i++) {
-		arrayObj.push(a1[i].attributes.file.value + ' \n' + '</br>')
+		arrayObj.push(a1[i].attributes.src.value + ' \n' + '</br>')
 	}
 	//消除分割符的逗号
 	str = arrayObj.toString().replace(/,/g, '');
@@ -20,7 +53,7 @@ function btn() {
 }
 
 //nsfwPicx
-function nsfwPicCom(){
+function nsfwPicCom() {
 	var x = document.getElementById("input")
 	y = x.value
 	$('#shu').html(y)
@@ -38,18 +71,18 @@ function nsfwPicCom(){
 }
 
 //xsnvshen
-function xsnvshen(){
+function xsnvshen() {
 	var x = document.getElementById("input")
 	y = x.value
 	$('#shu').html(y)
 	$('tr').hide();
 	a1 = document.getElementsByTagName("img");
 	//console.log(a1);
-		console.log(a1[0].getAttribute("data-original"));
+	console.log(a1[0].getAttribute("data-original"));
 	var arrayObj = new Array();
 	arrayObj.push(a1.length + ' \n' + '</br>');
 	for (i = 0; i < a1.length; i++) {
-		arrayObj.push("https:"+a1[i].getAttribute("data-original") + ' \n' + '</br>')
+		arrayObj.push("https:" + a1[i].getAttribute("data-original") + ' \n' + '</br>')
 	}
 	//消除分割符的逗号
 	str = arrayObj.toString().replace(/,/g, '');
@@ -103,3 +136,37 @@ function showSamllBtn() {
 }
 
 
+function isBrowser() {
+	var agent = navigator.userAgent.toLowerCase();
+	if (agent.indexOf('chrome') > 0) {
+		type = 'chrome'
+	}
+	if (agent.indexOf('firefox') > 0) {
+		type = 'firefox'
+	}
+	if (agent.indexOf('trident') > 0) {
+		type = 'trident'
+	}
+}
+
+
+function calculate(){
+	var openNum = document.getElementById("input_open").value
+	var closeNum = document.getElementById("input_close").value
+	var differDelPer
+	var differAddPer
+	if(openNum > closeNum){
+		var differDel =openNum-closeNum;
+		var differDelD = differDel/openNum;
+		differDelPer = (differDelD*100).toFixed(2);	
+	}else{
+		var differAdd = closeNum-openNum;
+		var differAddD = differAdd/openNum;
+		differAddPer =  (differAddD*100).toFixed(2);
+	}
+	if(differAddPer != null){
+		document.getElementById("calculate_text").innerHTML  = "<span style='color: red;'>+"+differAddPer+"</span>";
+	}else{
+		document.getElementById("calculate_text").innerHTML  = "<span style='color: green;'>-"+differDelPer+"</span>";
+	}
+}
